2ad2328d571ba45d0b9273346044988b6170b7a9
ikamil1998 <i.kamil@bdassmart.com>
2023-06-22 20:03:51 +0300

    add delete user

--- user/v1/api/users.controller.mjs
+++ user/v1/api/users.controller.mjs
@@ -4,6 +4,8 @@ import { createToken } from "../../../middlewares/user.auth.mjs"
 import UserProvider from "./users.provider.mjs"
 import {compare} from "bcrypt"
 import { BadRequestError, NotFoundError, UnauthorizedError } from "../../../shared/app-error.mjs";
+import TodoProvider from "../../../todo/v1/api/todo.provider.mjs"
+
 
 export default class UserController {
     static async signUp(req) {
@@ -74,6 +76,7 @@ export default class UserController {
         const filters = {_id}
         const [deletedUser, deletedTodo] = await Promise.all([
             UserProvider.deleteOne(filters),
+            TodoProvider.deleteMany({userId: _id})
         ])
         return {deletedUser}
     }
